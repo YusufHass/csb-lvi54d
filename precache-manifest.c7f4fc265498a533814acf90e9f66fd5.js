215be3797b891107f09bbede2c65a86b7744c046
Code Sandbox <hello@codesandbox.io>
2022-08-28 15:13:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c7f4fc265498a533814acf90e9f66fd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425acb5ebe3c4a4d39c7d2feafd71300",
+    "url": "https://yusufhass.github.io/csb-lvi54d/index.html"
+  },
+  {
+    "revision": "02635558fa974fbfee48",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/css/2.ab1f6e32.chunk.css"
+  },
+  {
+    "revision": "d3479c23e640f7fcfc61",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/css/main.3f01f47f.chunk.css"
+  },
+  {
+    "revision": "02635558fa974fbfee48",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/js/2.2b2986c4.chunk.js"
+  },
+  {
+    "revision": "e7130adb81c584f2748407b76feeff0d",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/js/2.2b2986c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3479c23e640f7fcfc61",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/js/main.8bd53c63.chunk.js"
+  },
+  {
+    "revision": "7015fcfc19090177f547",
+    "url": "https://yusufhass.github.io/csb-lvi54d/static/js/runtime-main.ad5ee28f.js"
+  }
+]);
